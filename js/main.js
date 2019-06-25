648c198aa23144236a525c75f6069ee68afab0bd
AnanyevAlex <a1113035@yandex.ru>
2019-06-25 21:11:43 +0500

    поправил рассчет X

--- js/main.js
+++ js/main.js
@@ -209,12 +209,13 @@ var addTimeChangeHandler = function (timeSelectField, relatedTimeSelectField) {
       };
 
       var pinYCoord = Math.max(MAP_Y_MIN_VALUE - MAP_MAIN_PIN_HEIGHT, Math.min(MAP_Y_MAX_VALUE - MAP_MAIN_PIN_HEIGHT, (mainMapPinEl.offsetTop - shift.y)));
+      var pinXCoord = Math.max(MAP_X_MIN_VALUE - MAP_MAIN_PIN_WIDTH / 2, Math.min(MAP_X_MAX_VALUE - MAP_MAIN_PIN_WIDTH / 2, (mainMapPinEl.offsetLeft - shift.x)));
 
       mainMapPinEl.style.top = pinYCoord + 'px';
-      mainMapPinEl.style.left = (mainMapPinEl.offsetLeft - shift.x) + 'px';
+      mainMapPinEl.style.left = pinXCoord + 'px';
 
       var posY = Math.floor((mainMapPinEl.offsetTop - shift.y) + MAP_MAIN_PIN_HEIGHT);
-      var posX = Math.floor((mainMapPinEl.offsetLeft - shift.x) - MAP_MAIN_PIN_WIDTH / 2);
+      var posX = Math.floor((mainMapPinEl.offsetLeft - shift.x) + MAP_MAIN_PIN_WIDTH / 2);
 
       inputAddressEl.value = posX + ', ' + posY;
     };
