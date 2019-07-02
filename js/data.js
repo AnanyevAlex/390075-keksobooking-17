31eaf882c2b0a3c359b8b1a6cf5b55064ed769b3
AnanyevAlex <a1113035@yandex.ru>
2019-07-02 17:36:11 +0500

    добавил вывод ошибки и проверку наличия offer

--- js/data.js
+++ js/data.js
@@ -1,53 +1,27 @@
 'use strict';
 (function () {
   var ADS_COUNT = 8;
-  var AVATAR_IMG_EXTENSION = '.png';
-  var AVATAR_IMG_SRC = 'img/avatars/user';
-  var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-  var MAP_PIN_WIDTH = 50;
-  var MAP_PIN_HEIGHT = 70;
   var MAP_Y_MAX_VALUE = 630;
   var MAP_Y_MIN_VALUE = 130;
   var MAP_X_MAX_VALUE = 1200;
   var MAP_X_MIN_VALUE = 0;
 
-  var getRandomNumber = function (max, min) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  var getAvatarImgSrc = function (numberads) {
-    return AVATAR_IMG_SRC + '0' + numberads + AVATAR_IMG_EXTENSION;
-  };
-
-  var getTypeOffer = function () {
-    return OFFER_TYPES[getRandomNumber(3, 0)];
-  };
-
   var getMapPinEl = function () {
     return document.querySelector('.map__pins');
   };
 
-  var generateAds = function () {
+  var getErrorMessageEl = function () {
+    var errorMessageEl = document.querySelector('#error')
+    .content
+    .querySelector('.error');
 
-    var ads = [];
-
-    for (var i = 0; i < ADS_COUNT; i++) {
-      ads.push({
-        avatar: getAvatarImgSrc(i + 1),
-        offer: getTypeOffer(),
-        location: {
-          x: getRandomNumber(MAP_X_MAX_VALUE, MAP_X_MIN_VALUE) - (MAP_PIN_WIDTH / 2),
-          y: getRandomNumber(MAP_Y_MAX_VALUE, MAP_Y_MIN_VALUE) - MAP_PIN_HEIGHT,
-        }
-      });
-    }
-
-    return ads;
+    return errorMessageEl;
   };
 
   window.data = {
-    generateAds: generateAds,
+    ADS_COUNT: ADS_COUNT,
     getMapPinEl: getMapPinEl,
+    getErrorMessageEl: getErrorMessageEl,
     MAP_Y_MAX_VALUE: MAP_Y_MAX_VALUE,
     MAP_Y_MIN_VALUE: MAP_Y_MIN_VALUE,
     MAP_X_MAX_VALUE: MAP_X_MAX_VALUE,
--- js/pin.js
+++ js/pin.js
@@ -2,12 +2,13 @@
 (function () {
   var addPinInfo = function (ad, template) {
     var pinElement = template.cloneNode(true);
-    pinElement.style.left = ad.location.x + 'px';
-    pinElement.style.top = ad.location.y + 'px';
-    var avatarImageEl = pinElement.querySelector('img');
-    avatarImageEl.src = ad.author.avatar;
-    avatarImageEl.alt = ad.offer.title;
-
+    if (ad.offer) {
+      pinElement.style.left = ad.location.x + 'px';
+      pinElement.style.top = ad.location.y + 'px';
+      var avatarImageEl = pinElement.querySelector('img');
+      avatarImageEl.src = ad.author.avatar;
+      avatarImageEl.alt = ad.offer.title;
+    }
     return pinElement;
   };
 
@@ -19,49 +20,29 @@
     return templatePinEl;
   };
 
-  // var generatePins = function () {
-  //   var arrayData;
-  //   var onError = function (message) {
-  //     console.error(message);
-  //   };
-  //   var onSuccess = function (data) {
-  //     debugger
-  //     arrayData = data
-  //     return arrayData;
-  //     console.log(arrayData);
-  //   }
-
-  //   window.load('https://js.dump.academy/keksobooking/data', onSuccess, onError);
-  //   var templatePinEl = getTemplatePinEl();
-  //   var ads = window.data.generateAds();
-  //   var fragmentPin = document.createDocumentFragment();
-
-  //   for (var k = 0; k < ads.length; k++) {
-  //     fragmentPin.appendChild(addPinInfo(ads[k], templatePinEl));
-  //   }
-
-  //   return fragmentPin;
-  // };
-var generatePins = function () {
-  window.load(function (pin) {
+  var successHandler = function (pin) {
     var templatePinEl = getTemplatePinEl();
     var mapPinEl = window.data.getMapPinEl();
     var fragmentPin = document.createDocumentFragment();
-    console.log(pin)
-    for (var k = 0; k < 8; k++) {
+    for (var k = 0; k < window.data.ADS_COUNT; k++) {
       fragmentPin.appendChild(addPinInfo(pin[k], templatePinEl));
     }
     mapPinEl.appendChild(fragmentPin);
     return mapPinEl;
+  };
 
-  })
-}
+  var errorHandler = function (errorMessage) {
+    var errorBlockEl = window.data.getErrorMessageEl();
+    var mapBlock = window.mapAction.mapBlockEl;
+    var errorBlock = errorBlockEl.cloneNode(true);
+    var errorMessageText = errorBlock.querySelector('.error__message');
+    errorMessageText.textContent = errorMessage;
+    mapBlock.appendChild(errorBlock);
+  };
 
-// var addMapPin = function () {
-//     var mapPinEl = window.data.getMapPinEl();
-//     console.log(generatePins())
-//     mapPinEl.appendChild(generatePins());
-//   };
+  var generatePins = function () {
+    window.load(successHandler, errorHandler);
+  };
 
   window.pin = {
     generatePins: generatePins,
