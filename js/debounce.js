c922776c116bf4f9b8825521d54c589a90882884
AnanyevAlex <a1113035@yandex.ru>
2019-07-25 16:56:28 +0500

    разбираюсь с фильтром

---
+++ js/debounce.js
@@ -0,0 +1,16 @@
+'use strict';
+(function () {
+  var DEBOUNCE_TIMEOUT = 500;
+  var lastTimeout = null;
+
+  var debounce = function (cb) {
+    if (lastTimeout) {
+      clearTimeout(lastTimeout);
+    }
+    lastTimeout = setTimeout(cb, DEBOUNCE_TIMEOUT);
+  };
+
+  window.debounce = {
+    debounce: debounce,
+  }
+})();
--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,13 @@
 'use strict';
 (function () {
+  var MIN_PRICE = 10000;
+  var MAX_PRICE = 50000;
   var housingTypeEl = document.querySelector('#housing-type');
+  var housingRoomEl = document.querySelector('#housing-rooms');
+  var housingGuestEl = document.querySelector('#housing-guests');
+  var housingPriceEl = document.querySelector('#housing-price');
+  var selectedFeaturesEl = document.querySelectorAll('input[name="features"]:checked');
+
   var mapPinsEl = document.querySelector('.map__pins');
   var adsArr = [];
 
@@ -16,16 +23,29 @@
   };
 
   var filterPins = function () {
+    // фильтр тип жилья
     housingTypeEl.addEventListener('change', function () {
+
       removePins();
       if (housingTypeEl.value === 'any') {
         window.pin.drawPins(adsArr);
       }
-
       var filteredAdsByType = adsArr.filter(function (it) {
         return it.offer.type === housingTypeEl.value;
       });
       window.pin.drawPins(filteredAdsByType);
+    });
+    // фильтр количество комнат
+    housingRoomEl.addEventListener('change', function () {
+
+      removePins();
+      if (housingRoomEl.value === 'any') {
+        window.pin.drawPins(adsArr);
+      }
+      var filteredAdsByRooms = adsArr.filter(function (it) {
+        return it.offer.rooms === Number(housingRoomEl.value);
+      });
+      window.pin.drawPins(filteredAdsByRooms);
 
     });
   };
