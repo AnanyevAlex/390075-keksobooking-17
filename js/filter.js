ab71db4bc2d3aa8a8514f45d2aa47538255a663f
AnanyevAlex <a1113035@yandex.ru>
2019-07-26 15:47:25 +0500

    перенос

--- js/filter.js
+++ js/filter.js
@@ -7,6 +7,20 @@
   var housingRoomEl = document.querySelector('#housing-rooms');
   var housingGuestEl = document.querySelector('#housing-guests');
   var featuresButtonsEl = document.querySelectorAll('input[name="features"]');
+  var mapFiltersEl = document.querySelector('.map__filters');
+  // дефолтный фильтр
+  var housingType = 'any';
+  var housingPrice = 'any';
+  var housingRooms = 'any';
+  var housingGuests = 'any';
+  var isWifi = false;
+  var isDishwasher = false;
+  var isParking = false;
+  var isWasher = false;
+  var isElevator = false;
+  var isConditioner = false;
+  var backupData;
+  var filters = [];
 
 
   var mapPinsEl = document.querySelector('.map__pins');
@@ -23,7 +37,167 @@
     });
   };
 
-  var filterPins = function () {
+
+
+  var checkFeature = function (featureButton) {
+    return featureButton.checked;
+  };
+
+  var removeFilter = function (element) {
+    var index = filters.indexOf(element);
+    if (index > -1) {
+      filters.splice(index, 1);
+    }
+  };
+
+  var fillFilterArray = function (filterName, filterValue) {
+    if ((filterValue === 'any') || (filterValue === false)) {
+      removeFilter(filterName);
+    } else {
+      var index = filters.indexOf(filterName);
+      if (index > -1) {
+        removeFilter(filterName);
+        filters.push(filterName);
+      } else {
+        filters.push(filterName);
+      }
+    }
+  };
+
+  // селекты
+
+  mapFiltersEl.addEventListener('change', function (evt) {
+    debugger
+    var target = evt.target;
+    switch (target.dataset.filter) {
+      case 'type':
+        housingType = target.value;
+        fillFilterArray(filterType, housingType);
+        break;
+      case 'price':
+        housingPrice = target.value;
+        fillFilterArray(filterPrice, housingPrice);
+        break;
+      case 'rooms':
+        housingRooms = target.value;
+        fillFilterArray(filterRooms, housingRooms);
+        break;
+      case 'guests':
+        housingGuests = target.value;
+        fillFilterArray(filterGuests, housingGuests);
+        break;
+      case 'wifi':
+        isWifi = checkFeature(featureWifi);
+        fillFilterArray(filterWifi, isWifi);
+        break;
+      case 'dishwasher':
+        isDishwasher = checkFeature(featureDishwasher);
+        fillFilterArray(filterDishwasher, isDishwasher);
+        break;
+      case 'parking':
+        isParking = checkFeature(featureParking);
+        fillFilterArray(filterParking, isParking);
+        break;
+      case 'washer':
+        isWasher = checkFeature(featureWasher);
+        fillFilterArray(filterWasher, isWasher);
+        break;
+      case 'elevator':
+        isElevator = checkFeature(featureElevator);
+        fillFilterArray(filterElevator, isElevator);
+        break;
+      case 'conditioner':
+        isConditioner = checkFeature(featureConditioner);
+        fillFilterArray(filterConditioner, isConditioner);
+        break;
+    }
+    window.debounce.debounce(function () {
+      removePins();
+      window.card.removeCardBlock();
+      window.updatePins();
+    });
+  });
+
+
+  // functions of selector filters
+  var filterType = function (it) {
+    return it.offer.type === housingType;
+  };
+
+  var filterPrice = function (it) {
+    if (housingPrice === 'low') {
+      return it.offer.price <= MIN_PRICE;
+    } else if (housingPrice === 'middle') {
+      return it.offer.price > MIN_PRICE && it.offer.price < MAX_PRICE;
+    } else if (housingPrice === 'high') {
+      return it.offer.price >= MAX_PRICE;
+    }
+    return true;
+  };
+
+  var filterRooms = function (it) {
+    return housingRooms === it.offer.rooms.toString();
+  };
+
+  var filterGuests = function (it) {
+    return housingGuests === it.offer.guests.toString();
+  };
+
+  // universal function of filtering any feature
+  var filterFeature = function (featureName) {
+    var filterFunction = function (it) {
+      return it.offer.features.indexOf(featureName) > -1;
+    };
+    return filterFunction;
+  };
+
+  var filterWifi = filterFeature('wifi');
+  var filterDishwasher = filterFeature('dishwasher');
+  var filterParking = filterFeature('parking');
+  var filterWasher = filterFeature('washer');
+  var filterElevator = filterFeature('elevator');
+  var filterConditioner = filterFeature('conditioner');
+
+  /* UPDATE PINS */
+
+  window.updatePins = function (data) {
+
+    // backup of data;
+    if (data) {
+      backupData = data;
+    }
+
+    /* !!!! In the fallowing function I didn't change the code,
+    because by meaning the cycle should not stop on the first
+    true/false, but only on false. */
+
+    var result = adsArr.filter(function (elem) {
+      for (var i = 0; i < filters.length; i++) {
+        if (filters[i](elem) === false) {
+          return false;
+        }
+      }
+      return true;
+    });
+
+    window.pin.drawPins(result);
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  /*var filterPins = function () {
     // фильтр тип жилья
     housingTypeEl.addEventListener('change', function () {
 
@@ -107,11 +281,11 @@ console.log(selectedFeaturesValues)
         if (input.checked) {
           var filteredAdsByFeatures = adsArr.filter(function (it) {
             var featuresInAd = it.offer.features;
-            /*for (var i = 0; i < selectedFeaturesValues.length; i++) {
+            for (var i = 0; i < selectedFeaturesValues.length; i++) {
               if (featuresInAd.includes(input.value)) {
                 return it;
               }
-            }*/
+            }
             featuresInAd.includes(selectedFeaturesValues)
           });
           window.pin.drawPins(filteredAdsByFeatures);
@@ -122,10 +296,10 @@ console.log(selectedFeaturesValues)
 
     });
 
-  };
+  };*/
 
   window.filter = {
-    filterPins: filterPins,
+    /*filterPins: filterPins,*/
     setAdsArr: setAdsArr,
     removePins: removePins,
   };
